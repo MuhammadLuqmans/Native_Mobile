cac8078aa26ce6a9e25f1f1dcf867f8282cfaa55
MuhammadLuqmans <MuhammadLuqmans>
2022-03-20 15:56:05 +0500

    native-chat

--- App.js
+++ App.js
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Pressable,
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function ScreenA({ navigation }) {
+
+  const onPressHandler = () => {
+    navigation.navigate('Screen_B');
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.body}>
+      <Text style={styles.text}>
+        Screen A
+      </Text>
+      <Pressable
+        onPress={onPressHandler}
+        style={({ pressed }) => ({ backgroundColor: pressed ? '#ddd' : '#0ff' })}
+      >
+        <Text style={styles.text}>
+          Go to Screen B
+        </Text>
+      </Pressable>
     </View>
-  );
+  )
+}
+
+function ScreenB({ navigation }) {
+
+  const onPressHandler = () => {
+    // navigation.navigate('Screen_A');
+    navigation.goBack();
+  }
+
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>
+        Screen B
+      </Text>
+      <Pressable
+        onPress={onPressHandler}
+        style={({ pressed }) => ({ backgroundColor: pressed ? '#ddd' : '#0ff' })}
+      >
+        <Text style={styles.text}>
+          Go Back to Screen A
+        </Text>
+      </Pressable>
+    </View>
+  )
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        // screenOptions={{
+        //   header: () => null
+        // }}
+      >
+        <Stack.Screen
+          name="Screen_A"
+          component={ScreenA}
+        // options={{
+        //   header: () => null
+        // }}
+        />
+        <Stack.Screen
+          name="Screen_B"
+          component={ScreenB}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  text: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    margin: 10,
+  }
+})
+
+export default App;
